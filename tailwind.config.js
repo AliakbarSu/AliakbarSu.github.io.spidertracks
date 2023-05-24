860cd1d32fed02d5dae8df96bb9e50afcc0e7ae0
Ali Sultani <aliakbar.su@hotmail.com>
2023-05-25 10:22:43 +1200

    Install & configure tailwindui

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef, @typescript-eslint/no-var-requires
+const defaultTheme = require('tailwindcss/defaultTheme')
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans]
+      }
+    }
+  },
+  plugins: []
+}
