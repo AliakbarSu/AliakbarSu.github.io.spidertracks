8a3e5a63206fa4a895e0f8269d2e3dbb38d2318b
Ali Sultani <aliakbar.su@hotmail.com>
2023-05-26 08:29:08 +1200

    Add api lib

---
+++ src/library/api.ts
@@ -0,0 +1,69 @@
+import { CustomerStatusEnum } from 'types/Customer'
+import { OpportunityStatusEnum } from 'types/Opportunity'
+import axios from 'axios'
+
+export const updateCustomerStatusApi = async ({
+  id,
+  status
+}: {
+  id: string
+  status: CustomerStatusEnum
+}): Promise<string> => {
+  const result = await axios.put(
+    `${import.meta.env.VITE_API_URL}/customers/${id}/update`,
+    { status },
+    {
+      headers: { 'Content-Type': 'application/json' }
+    }
+  )
+  return result.data
+}
+
+export const addOpportunityApi = async ({
+  customerId,
+  name,
+  status
+}: {
+  customerId: string
+  name: string
+  status: OpportunityStatusEnum
+}): Promise<string> => {
+  const result = await axios.post(
+    `${import.meta.env.VITE_API_URL}/customers/${customerId}/opportunities/add`,
+    { name, status },
+    {
+      headers: { 'Content-Type': 'application/json' }
+    }
+  )
+  return result.data
+}
+
+export const updateOpportunityApi = async ({
+  customerId,
+  id,
+  name,
+  status
+}: {
+  customerId: string
+  id: string
+  name: string
+  status: OpportunityStatusEnum
+}): Promise<string> => {
+  const result = await axios.put(
+    `${
+      import.meta.env.VITE_API_URL
+    }/customers/${customerId}/opportunities/update`,
+    { id, name, status },
+    {
+      headers: { 'Content-Type': 'application/json' }
+    }
+  )
+  return result.data
+}
+
+export const getCustomers = async () => {
+  const result = await axios.get(`${import.meta.env.VITE_API_URL}/customers`, {
+    headers: { 'Content-Type': 'application/json' }
+  })
+  return result.data
+}
