0a30d4003d757ed75737c77ba5b1a676d0950dd3
Ali Sultani <aliakbar.su@hotmail.com>
2023-05-26 08:08:46 +1200

    Configure absolute paths

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,21 @@
 module.exports = {
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: [path.resolve(__dirname)]
+      }
+    }
+  },
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    'plugin:react-hooks/recommended'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': 'warn',
-  },
+    'react-refresh/only-export-components': 'warn'
+  }
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, './src'),
+      types: path.resolve(__dirname, './src/types'),
+      libs: path.resolve(__dirname, './src/library'),
+      components: path.resolve(__dirname, './src/components'),
+      store: path.resolve(__dirname, './src/store'),
+      hooks: path.resolve(__dirname, './src/hooks')
+    }
+  }
 })
