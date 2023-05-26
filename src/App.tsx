befb280ba84abfe5a4245466d88890764c9f6076
Ali Sultani <aliakbar.su@hotmail.com>
2023-05-26 18:17:17 +1200

    Add filters

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,8 @@ import './App.css'
 import Customers from './views/Customers'
 
 function App() {
-
   return (
-    <>
-      <div className="border-b border-gray-200 pb-5">
-        <h3 className="text-base font-semibold leading-6 text-gray-900">Customers</h3>
-      </div>
-      <Customers />
-    </>
+    <Customers />
   )
 }
 
--- src/views/Customers.tsx
+++ src/views/Customers.tsx
@@ -1,9 +1,10 @@
-import { Customer } from "../types/Customer"
-import CustomerComponent from "../components/Customer"
+import { Customer, CustomerStatusEnum } from "../types/Customer"
+import CustomerComponent from "components/Customer"
 import { useLoaderData } from "react-router-dom"
 import { useAppDispatch, useAppSelector } from "hooks/store"
 import { selectCustomers, setCustomers } from "store/customer"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+import Filters from "components/Filters"
 
 // const customers: Customer[] = [
 //     {
@@ -72,15 +73,29 @@ export default function Customers() {
     const dispatch = useAppDispatch()
     const customers = useLoaderData() as Customer[]
     const data = useAppSelector(selectCustomers)
+    const [filters, setFilters] = useState<CustomerStatusEnum[]>([CustomerStatusEnum.active, CustomerStatusEnum.inactive, CustomerStatusEnum.lead])
+    const [sortOrder, setSortOrder] = useState<number>(1)
+
+    const filterAndSort = (
+    ): Customer[] => {
+        const filteredItems = customers.filter((item) => filters.includes(item.status));
+        return filteredItems.sort((a, b) => {
+            return a.name.localeCompare(b.name) ? sortOrder : -sortOrder;
+        });
+    }
 
     useEffect(() => {
-        dispatch(setCustomers(customers))
-    }, [customers])
+        const sortedCustomers = filterAndSort()
+        dispatch(setCustomers(sortedCustomers))
+    }, [customers, filters, sortOrder])
     return (
-        <ul role="list" className="divide-y divide-gray-100">
-            {data.map((customer) => (
-                <CustomerComponent key={customer.id} customer={customer} />
-            ))}
-        </ul>
+        <>
+            <Filters onSort={setSortOrder} onFilters={setFilters} />
+            <ul role="list" className="divide-y divide-gray-100">
+                {data.map((customer) => (
+                    <CustomerComponent key={customer.id} customer={customer} />
+                ))}
+            </ul>
+        </>
     )
 }
